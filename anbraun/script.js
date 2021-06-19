323dc618570686d71e2b746ffbaae318c83bf065
Tim Göppel <81766887+TimGoeppel@users.noreply.github.com>
2021-06-19 12:39:06 +0200

    ANbraun aktualisieren

--- anbraun/script.js
+++ anbraun/script.js
@@ -2,4 +2,6 @@ const year = new Date().getFullYear();
 const copyright = document.getElementById("copyright");
 if(copyright) copyright.innerHTML = "2020-" + year;
 const jahr = document.getElementById("jahr");
-if(jahr) jahr.innerHTML = year - 111;
+if(jahr) jahr.innerHTML = 1909;
+const jahre = document.getElementById("jahre");
+if(jahre) jahre.innerHTML = year - 1909;
